1667767bd23da4013a7fe4bf0023bd0f81d3a252
lawvs <lawvs@users.noreply.github.com>
2019-09-14 02:23:54 +0900

    chore: setup env

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// https://prettier.io/docs/en/install.html
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  // bracketSpacing: true,
+  // tabWidth: 2,
+  // useTabs: false,
+}
