d29ab6e3f2e234458824c6b0dfd8b408094a195e
lawvs <lawvs@users.noreply.github.com>
2019-09-17 00:36:52 +0900

    chore: update app

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import { GlobalStyle } from './styles'
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  line-height: 1.5;
+`
 
 export const App = () => {
   return (
     <>
       <GlobalStyle />
-      <div>Hello</div>
+      <Container>
+        <Wrapper>Hello</Wrapper>
+      </Container>
     </>
   )
 }
