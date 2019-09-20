dd8455a2c51e7e9953cc15e0b76b69dcf670c8c6
lawvs <lawvs@users.noreply.github.com>
2019-09-21 03:37:55 +0900

    fix: horizontal scroll bar overflow

--- src/app.tsx
+++ src/app.tsx
@@ -6,9 +6,9 @@ import { GlobalStyle } from './styles'
 const Container = styled.div`
   height: 100vh;
   width: 100vw;
+  max-width: 100%;
   display: flex;
   flex-direction: column;
-  overflow: hidden;
 `
 
 const Wrapper = styled.div`
