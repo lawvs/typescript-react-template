b6b17045838654d1ede895099d9ec93786d35f3f
lawvs <lawvs@users.noreply.github.com>
2020-04-03 04:51:51 +0900

    refactor: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
 // https://eslint.org/
+
+/** @type { import("@types/eslint").Linter.Config } */
 module.exports = {
   env: {
     es6: true,
@@ -7,36 +9,40 @@ module.exports = {
     // jest: true,
   },
   parser: '@typescript-eslint/parser',
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'eslint:recommended',
-    'plugin:react/recommended',
-  ],
   plugins: ['@typescript-eslint', 'react', 'prettier'],
+  extends: [
+    'airbnb',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: 'tsconfig.json',
     ecmaFeatures: {
       jsx: true,
     },
   },
   rules: {
-    // semi: ['error', 'never'],
-    // 'comma-dangle': ['error', 'always-multiline'],
-    '@typescript-eslint/indent': 0,
-    '@typescript-eslint/camelcase': 0,
-    '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'none' } }],
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/no-explicit-any': 'off',
-    'linebreak-style': ['error', 'unix'],
     'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'no-undef': 'error',
-    'no-var': 'error',
-    'no-unused-vars': ['error', { args: 'none' }],
-    'unicode-bom': 'error',
-    'prefer-const': ['error', { destructuring: 'all' }],
     'prettier/prettier': 'warn',
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
   settings: {
     react: {
