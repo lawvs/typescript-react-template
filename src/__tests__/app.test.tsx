318db5e3d8ea76ba2396252c437a63bbe938eda7
lawvs <lawvs@users.noreply.github.com>
2020-05-15 00:09:20 +0900

    chore: update directory structure

--- src/__tests__/app.test.tsx
+++ src/__tests__/app.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { App } from '../src/app'
+import { App } from '../app'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
