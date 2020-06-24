74f42d7a9005d491cb8e7d306d644f908e04d77b
lawvs <lawvs@users.noreply.github.com>
2020-06-24 18:01:33 +0800

    fix: jest handling static assets

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,8 @@ module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
   testMatch: ['**/__tests__/**/*.{ts,tsx}', '**/?(*.)+(spec|test).{ts,tsx}'],
   collectCoverageFrom: ['./src/**/*.{js,ts,tsx}'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+  },
 }
