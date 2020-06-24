f79d3381ddc7d623197ff27f7ed1d1db45611bbe
lawvs <lawvs@users.noreply.github.com>
2020-06-24 18:21:07 +0800

    chore: enhance jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,16 @@
-module.exports = {
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ * See https://jestjs.io/docs/en/configuration
+ */
+const jestConfig = {
   preset: 'ts-jest',
   clearMocks: true,
   coverageDirectory: 'coverage',
-  moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
-  testMatch: ['**/__tests__/**/*.{ts,tsx}', '**/?(*.)+(spec|test).{ts,tsx}'],
   collectCoverageFrom: ['./src/**/*.{js,ts,tsx}'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
   },
 }
+
+module.exports = jestConfig
