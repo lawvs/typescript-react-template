1edd4d948487f2aed0fb450431d19f8ebc53a881
lawvs <18554747+lawvs@users.noreply.github.com>
2022-01-04 16:51:09 +0800

    chore: update jest to v27

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from '@jest/types'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defaults as tsjPreset } from 'ts-jest/presets'
 
 const jestConfig: Config.InitialOptions = {
   preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    ...tsjPreset.transform,
+  },
   clearMocks: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['./src/**/*.{js,ts,tsx}'],
